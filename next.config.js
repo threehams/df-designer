6321758e5d786885c86c7988e5970724e9aa57e9
David Edmondson <threehams@gmail.com>
2018-11-26 18:23:59 -0800

    Fix now deployment

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
-const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+const { PHASE_PRODUCTION_SERVER } = require("next-server/constants");
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {
+      /* production only config */
+    };
+  }
+  const withTypescript = require("@zeit/next-typescript");
+  return withTypescript();
+};
