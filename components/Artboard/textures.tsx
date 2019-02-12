958c00988ec7bc9c7e62420fc85b2b7653461cc0
David Edmondson <threehams@gmail.com>
2019-02-11 21:59:58 -0800

    Minor refactor, consolidate events for testing.

---
+++ components/Artboard/textures.tsx
@@ -0,0 +1,20 @@
+import { keys } from "../../lib/keys";
+import { tilesetNames } from "../../lib/tilesetNames";
+
+export const TILE_SIZE = 16;
+
+const spriteSheet = PIXI.BaseTexture.fromImage("/static/phoebus.png");
+export type TilesetMap = { [key in keyof typeof tilesetNames]: PIXI.Texture };
+export const textures = keys(tilesetNames).reduce(
+  (result, name) => {
+    const num = tilesetNames[name];
+    const x = (num % TILE_SIZE) * TILE_SIZE;
+    const y = Math.floor(num / TILE_SIZE) * TILE_SIZE;
+    result[name] = new PIXI.Texture(
+      spriteSheet,
+      new PIXI.Rectangle(x, y, TILE_SIZE, TILE_SIZE),
+    );
+    return result;
+  },
+  {} as TilesetMap,
+);
